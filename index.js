d580d285af2e64be05ab8c2ab4ab24b6cec48c65
gregorrr1993 <kawalecgrzegorz@o2.pl>
2022-04-30 21:34:45 +0200

    refactor: game object

--- index.js
+++ index.js
@@ -1,79 +1,76 @@
-var level = 1;
-var i = 0;
-var sequence = [];
-var started = false;
-var over = false;
+var index = 0;
 
 $(".btn").click(function () {
-    if (started) {
+    if (game.started) {
         var button = $(this);
         button.addClass("pressed");
         setTimeout(function () {
             button.removeClass("pressed");
         }, 100);
-        playSound(button);
-        if (button.attr("id") === sequence[i]) {
-            ++i;
+        game.playSound(button);
+        if (button.attr("id") === game.sequence[index]) {
+            ++index;
         }
-        else gameOver();
-        if (i >= level)
-        {
+        else game.gameOver();
+        if (index >= game.level) {
             setTimeout(function () {
-                if (!over) {
-                    nextLevel();
-                }                
-            }, 1000);           
+                if (!game.over) {
+                    game.nextLevel();
+                }
+            }, 1000);
         }
     }
 })
 
-$(document).keydown(function() {
-    if(!started) {
-        start();
+$(document).keydown(function () {
+    if (!game.started) {
+        game.start();
     }
 })
 
-function gameOver () {
-    var wrong = new Audio("sounds/wrong.mp3");
-    wrong.play();
-    $("body").addClass("game-over");
-    $("#level-title").text("Game Over, Press Any Key to Restart");
-    level = 1;
-    i = 0;
-    sequence = [];
-    started = false;
-    over = true;
-    setTimeout(function () {
-        $("body").removeClass("game-over");
-    }, 200);
-}
-
-function start () {
-    started = true;
-    over = false;
-    $("#level-title").text("Level " + level);
-    addToSequence(); 
-}
-
-function nextLevel () {
-    ++level;
-    $("#level-title").text("Level " + level);
-    addToSequence();
-    i = 0;
-}
-
-function playSound (button) {
-    var sound = new Audio ("sounds/" + button.attr("id") + ".mp3");
-    sound.play();
-}
-
-function addToSequence() {
-    var colors = ["green", "red", "yellow", "blue"];
-    var index = Math.floor(Math.random() * 4);
-    sequence.push(colors[index]);
-    var button = $("#" + colors[index])
-    button.animate({opacity: 0.5}, 50, function() {
-        button.animate({opacity: 1});
-    });
-    playSound(button);
+var game = {
+    level: 1,
+    sequence: [],
+    started: false,
+    over: false,
+    start () {
+        this.started = true;
+        this.over = false;
+        $("#level-title").text("Level " + this.level);
+        this.addToSequence();
+    },
+    nextLevel () {
+        ++this.level;
+        $("#level-title").text("Level " + this.level);
+        this.addToSequence();
+        index = 0;
+    },
+    addToSequence () {
+        var colors = ["green", "red", "yellow", "blue"];
+        var index = Math.floor(Math.random() * 4);
+        this.sequence.push(colors[index]);
+        var button = $("#" + colors[index])
+        button.animate({ opacity: 0.5 }, 50, function () {
+            button.animate({ opacity: 1 });
+        });
+        this.playSound(button);
+    },
+    playSound (button) {
+        var sound = new Audio("sounds/" + button.attr("id") + ".mp3");
+        sound.play();
+    },
+    gameOver () {
+        var wrong = new Audio("sounds/wrong.mp3");
+        wrong.play();
+        $("body").addClass("game-over");
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        this.level = 1;
+        index = 0;
+        this.sequence = [];
+        this.started = false;
+        this.over = true;
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        }, 200);
+    }
 }
